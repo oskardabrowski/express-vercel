07257c81be166fedbb5f7ab5e35e9d9f4784e3bd
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-05-28 22:36:07 +0200

    Second test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-app.listen(process.env.PORT || 3000);
+const path = require("path");
+
 app.use(express.static("public"));
 
-//index.js
 app.get("/", (req, res) => {
 	res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
-// index.js
+app.listen(process.env.PORT || 3000);
+
 module.exports = app;
