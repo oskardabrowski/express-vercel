f8a5d268c70896969a3db9380cdd016cdc91a119
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-05-28 22:18:08 +0200

    Nest step

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static("public"));
 
 //index.js
 app.get("/", (req, res) => {
-	res.sendFile("index.html", { root: path.join(__dirname, "public") });
+	res.send("Caps parser is working!");
 });
 
 app.listen(process.env.PORT || 3000);
