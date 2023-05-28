19fedd427fdc8c3d51307ce9f5dd3328932cc58c
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-05-28 22:58:21 +0200

    Enable CORS second time

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
 	res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
-
+app.options("*", cors());
 app.post("/getCapabilities", async (req, res) => {
 	const data = req.body;
 	if (data.url != null) {
